bf3d6dac495845bb63018e3d8bf9e3395528f975
rgo1482 <rgordon@carnival.com>
2025-08-28 15:31:23 -0400

    adding outputs for kube_config

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -14,6 +14,32 @@ output "kube_admin_config_raw" {
   sensitive   = true
 }
 
+output "kube_host" {
+  value       = azurerm_kubernetes_cluster.aks.kube_config[0].host
+  description = "AKS Kubernetes host"
+}
+
+output "kube_client_cert" {
+  value = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate)
+
+  sensitive   = true
+  description = "Base 64 decoded kubernetes client cert"
+}
+
+output "kube_key" {
+  value = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_key)
+
+  sensitive   = true
+  description = "Base64 decoded kubernetes key"
+}
+
+output "kube_ca_cert" {
+  value = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].cluster_ca_certificate)
+
+  sensitive   = true
+  description = "Base64 decoded kubernetes CS cert"
+}
+
 output "acr_login_server" {
   description = "The login server URL of the Azure Container Registry"
   value = azurerm_container_registry.acr.login_server
