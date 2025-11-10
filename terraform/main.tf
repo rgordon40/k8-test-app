64a1b25a6c1511dd92b8c534aed604a93af77816
rgo1482 <rgordon@carnival.com>
2025-11-10 17:29:55 -0500

    updating syspool name

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,7 +22,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   dns_prefix          = "${var.prefix}-dns"
 
   default_node_pool {
-    name       = "agentpool"
+    name       = "systempool"
     node_count = var.node_count
     vm_size    = var.node_vm_size
   }
